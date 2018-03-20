684e3d6d6f3713fc2e6582ba301bf874bd25622a
kamyar68 <kamyar.hasanzadeh@gmail.com>
2018-03-20 10:55:30 +0200

    license added

--- neighborhood_modeling_tools/IREM.py
+++ neighborhood_modeling_tools/IREM.py
@@ -150,6 +150,7 @@ else:
 # #################
 ## Convert lines to points
 # when this function is called the input line will be converted to a pointed line with equal intervals -as specified.
+
 def pointmaker(infc,outfc,intvl,uid_or_w):
     #print "running line to point tool"
     #arcpy.AddMessage("running line to point tool")
@@ -159,12 +160,7 @@ def pointmaker(infc,outfc,intvl,uid_or_w):
     use_percentage = "VALUE"
     end_points = "NO_END_POINTS"
     desc = arcpy.Describe(in_fc)
-# Create output feature class
-    # arcpy.CreateFeatureclass_management(
-    # os.path.dirname(out_fc),
-    # os.path.basename(out_fc),
-    # geometry_type="POINT",
-    # spatial_reference=desc.spatialReference)
+
     arcpy.CreateFeatureclass_management('in_memory',out_fc,"POINT")
     # Add a field to transfer FID from input
     if uid_or_w=='w':
@@ -172,7 +168,7 @@ def pointmaker(infc,outfc,intvl,uid_or_w):
         arcpy.AddField_management(out_fc, fid_name, "DOUBLE")
         # Create new points based on input lines
         with arcpy.da.SearchCursor(
-    # in_fc, ['SHAPE@', desc.OIDFieldName]) as search_cursor:
+    
             in_fc, ['SHAPE@', 'w_road']) as search_cursor:    
                 with arcpy.da.InsertCursor(
                     out_fc, ['SHAPE@', fid_name]) as insert_cursor:
@@ -205,7 +201,7 @@ def pointmaker(infc,outfc,intvl,uid_or_w):
         arcpy.AddField_management(out_fc, fid_name, "SHORT")
         # Create new points based on input lines
         with arcpy.da.SearchCursor(
-    # in_fc, ['SHAPE@', desc.OIDFieldName]) as search_cursor:
+    
             in_fc, ['SHAPE@', 'uid']) as search_cursor:    
                 with arcpy.da.InsertCursor(
                     out_fc, ['SHAPE@', fid_name]) as insert_cursor:
--- neighborhood_modeling_tools/test.py
+++
@@ -1,2 +0,0 @@
-import arcpy
-import pmkr
